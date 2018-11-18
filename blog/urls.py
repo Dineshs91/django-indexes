829486107de7ac5c00fa4e937da4d4f695fa98bd
dinesh <dineshpy07@gmail.com>
2018-11-18 08:28:33 +0530

    Add an example while getting a blog with site

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from blog.views import BlogListView, BlogDetailView, PostView
+from blog.views import BlogListView, BlogDetailView, PostView, BlogSiteView
 
 
 urlpatterns = [
     path("blogs/", BlogListView.as_view()),
     path("blogs/<int:blog_id>/", BlogDetailView.as_view()),
+    path("blogs/<str:site>/", BlogSiteView.as_view()),
     path("blogs/<int:blog_id>/posts/<int:post_id>/", PostView.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -28,23 +28,33 @@ class BlogDetailView(APIView):
         try:
             blog = Blog.objects.get(id=blog_id)
         except Blog.DoesNotExist:
-            return create_response(error={"message": "Blog does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+            return create_response(error={"message": "Blog does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = BlogSerializer(blog)
         return create_response(data=serializer.data, status=status.HTTP_200_OK)
 
+
+class BlogSiteView(APIView):
+    def get(self, request, site):
+        try:
+            blog = Blog.objects.get(site=site)
+        except Blog.DoesNotExist:
+            return create_response(error={"message": "Blog does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = BlogSerializer(blog)
+        return create_response(data=serializer.data, status=status.HTTP_200_OK)
 
 class PostView(APIView):
     def get(self, request, blog_id, post_id):
         try:
             blog = Blog.objects.get(id=blog_id)
         except Blog.DoesNotExist:
-            return create_response(error={"message": "Blog does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+            return create_response(error={"message": "Blog does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
         try:
             post = blog.blog_posts.get(id=post_id)
         except Post.DoesNotExist:
-            return create_response(error={"message": "Post does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+            return create_response(error={"message": "Post does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = PostSerializer(post)
         return create_response(data=serializer.data, status=status.HTTP_200_OK)
